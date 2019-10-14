c39ce8956e9bbdde4e16c70fa213cb2f39cd1092
Francis <francis@marasigan.net>
2019-10-14 14:00:03 -0700

    Completed Step 5

--- src/Francis.Calculator.Services.UnitTests/CalculatorServiceTests.cs
+++ src/Francis.Calculator.Services.UnitTests/CalculatorServiceTests.cs
@@ -14,11 +14,12 @@ namespace Francis.Calculator.Services.UnitTests
         
         [Theory]
         [InlineData("20", 20)]
-        [InlineData("1,5000", 5001)]
+        [InlineData("1,5000", 1)]
         [InlineData("", 0)]
         [InlineData("5, tytyt", 5)]
         [InlineData("1,2,3,4,5,6,7,8,9,10,11,12", 78)]
         [InlineData("1\n2,3", 6)]
+        [InlineData("2,1001,6", 8)]
         public void GetResultTest(string input, int expectedResult)
         {
             var result = _addCalculator.GetResult(input);
--- src/Francis.Calculator.Services/CalculatorService.cs
+++ src/Francis.Calculator.Services/CalculatorService.cs
@@ -6,6 +6,7 @@ namespace Francis.Calculator.Services
     public class CalculatorService
     {
         private static readonly char[] Delimeter = new[] {'\n', ','};
+        private const int MaxValue = 1000; 
 
         public double GetResult(string input)
         {
@@ -21,6 +22,7 @@ namespace Francis.Calculator.Services
                 throw new ArgumentException(string.Join(",",  negativeValues));
             
             var answer = values
+                .Where(a => a <= 1000)
                 .Sum();
  
             return answer;
